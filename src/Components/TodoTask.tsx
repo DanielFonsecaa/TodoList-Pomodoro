a3b5d9225f8ee87d75ad770f4d7d88010d0a5878
daniel <danieldafonseca@sapo.pt>
2025-02-11 16:05:28 +0000

    fixing bugs to deploy mvp

--- src/Components/TodoTask.tsx
+++ src/Components/TodoTask.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { ITask } from "../Interface";
 
 interface Props {
--- src/pages/CompleteTask.tsx
+++ src/pages/CompleteTask.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { ITask } from "../Interface";
 import TodoTask from "../Components/TodoTask";
 
